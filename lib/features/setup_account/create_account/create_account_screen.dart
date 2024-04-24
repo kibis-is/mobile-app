8256a560b15ed84c55b2de96e41fcae6e12554a5
David Welsh <contact@davidwelsh.net>
2024-04-24 11:12:38 +0700

    Added widgets to create account screen

--- lib/features/setup_account/create_account/create_account_screen.dart
+++ lib/features/setup_account/create_account/create_account_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kibisis/common_widgets/custom_button.dart';
 import 'package:kibisis/constants/constants.dart';
 
 class CreateAccountScreen extends ConsumerStatefulWidget {
@@ -14,12 +15,32 @@ class CreateAccountScreen extends ConsumerStatefulWidget {
 class _CreateAccountScreenState extends ConsumerState<CreateAccountScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Create New Account"),
+      ),
       body: Padding(
-        padding: EdgeInsets.all(kScreenPadding),
+        padding: const EdgeInsets.all(kScreenPadding),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Generate seed phrase',
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.onSecondary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: kScreenPadding,
+            ),
+            Text(
+                'Here is your 25 word mnemonic seed phrase. Make sure you save this in a secure place.',
+                style: Theme.of(context).textTheme.bodySmall),
+            CustomButton(
+              text: 'Copy',
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
     );
