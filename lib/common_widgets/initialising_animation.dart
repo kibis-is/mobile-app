0b9529e6cc039bb7f6540e94d32ce2ca7e864150
David Welsh <contact@davidwelsh.net>
2024-05-22 19:31:25 +0700

    initialise animation

---
+++ lib/common_widgets/initialising_animation.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AnimatedDots extends StatefulWidget {
+  const AnimatedDots({super.key});
+
+  @override
+  AnimatedDotsState createState() => AnimatedDotsState();
+}
+
+class AnimatedDotsState extends State<AnimatedDots>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<int> _dotCount;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    )..repeat();
+
+    _dotCount = IntTween(begin: 1, end: 3).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _dotCount,
+      builder: (context, child) {
+        String dots = '.' * _dotCount.value;
+        return Text(
+          'Initializing$dots',
+          style: Theme.of(context).textTheme.bodySmall,
+        );
+      },
+    );
+  }
+}
