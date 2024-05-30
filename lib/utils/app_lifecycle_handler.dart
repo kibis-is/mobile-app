4cd78dd281dfa3ca6cfc234828ef3659deee43b7
David Welsh <contact@davidwelsh.net>
2024-05-30 18:38:58 +0700

    fix pin locking bug

--- lib/utils/app_lifecycle_handler.dart
+++ lib/utils/app_lifecycle_handler.dart
@@ -1,6 +1,7 @@
 // app_lifecycle_handler.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kibisis/features/settings/providers/settings_providers.dart';
 import 'package:kibisis/providers/authentication_provider.dart';
 import 'package:kibisis/providers/account_provider.dart';
 import 'package:kibisis/providers/lock_timeout_provider.dart';
@@ -38,24 +39,20 @@ class AppLifecycleHandler with WidgetsBindingObserver {
   }
 
   void handleOnForeground() {
-    ref.read(lockTimeoutProvider);
+    final lockoutTime = ref.read(lockTimeoutProvider);
+
+    final isPasswordLockEnabled = ref.read(enablePasswordLockProvider);
     if (_backgroundTime != null) {
       final duration = DateTime.now().difference(_backgroundTime!);
       debugPrint(
           'App was in the background for: ${duration.inSeconds} seconds');
       _backgroundTime = null; // Reset the background time
 
-      // Check if the duration exceeds one minute
-      if (duration.inSeconds > 60) {
-        debugPrint('The app was in the background for more than one minute.');
-        // Call the onResumed callback with the duration if needed
-        onResumed?.call(duration.inSeconds);
+      onResumed?.call(duration.inSeconds);
 
-        // Additional actions here if the app was in the background for more than one minute
+      if (isPasswordLockEnabled && duration.inSeconds > lockoutTime) {
+        debugPrint('The app was in the background for more than one minute.');
         handleTimeout();
-      } else {
-        // Actions or callbacks if the duration is less than one minute
-        onResumed?.call(duration.inSeconds);
       }
     }
   }
