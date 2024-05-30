3c23528ae1c4dd49c763509203b50413dbd23d01
David Welsh <contact@davidwelsh.net>
2024-05-30 18:40:01 +0700

    Create remaining functions for algorand

--- lib/providers/algorand_provider.dart
+++ lib/providers/algorand_provider.dart
@@ -1,6 +1,6 @@
+import 'package:algorand_dart/algorand_dart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:algorand_dart/algorand_dart.dart';
 import 'package:kibisis/models/asset.dart';
 
 // Define a provider for managing the Algorand instance
@@ -30,28 +30,22 @@ class AlgorandService {
 
   AlgorandService(this.algorand);
 
-  Future<void> sendPayment(
-      String recipientAddress, double amount, Account account) async {
+  Future<String> sendPayment(Account senderAccount, Address recipientAddress,
+      double amountInAlgos) async {
     try {
-      final transactionId =
-          await executePaymentTransaction(recipientAddress, amount, account);
-      debugPrint("Transaction successful: $transactionId");
-    } catch (e) {
-      debugPrint("Failed to execute transaction: $e");
-    }
-  }
+      final amountInMicroAlgos = Algo.toMicroAlgos(amountInAlgos);
 
-  Future<String?> executePaymentTransaction(
-      String recipientAddress, double amount, Account account) async {
-    try {
-      return await algorand.sendPayment(
-        account: account,
-        recipient: Address.fromAlgorandAddress(address: recipientAddress),
-        amount: Algo.toMicroAlgos(amount),
+      final txId = await algorand.sendPayment(
+        account: senderAccount,
+        recipient: recipientAddress,
+        amount: amountInMicroAlgos,
       );
+
+      debugPrint("Payment successful: Transaction ID: $txId");
+      return txId;
     } catch (e) {
-      debugPrint("Failed to execute transaction: $e");
-      throw Exception("Failed to execute transaction: $e");
+      debugPrint("Failed to send payment: $e");
+      rethrow;
     }
   }
 
@@ -86,10 +80,133 @@ class AlgorandService {
       return Algo.fromMicroAlgos(balance).toString();
     } on AlgorandException catch (e) {
       debugPrint('Algorand Exception: ${e.message}');
-      return '0'; // Return 0 balance in case of AlgorandException
+      return '0';
     } catch (e) {
       debugPrint('General Exception: $e');
       throw Exception('Failed to get account balance: $e');
+    }
+  }
+
+  Future<String> createAsset(Account account, String assetName, String unitName,
+      int totalAssets, int decimals) async {
+    try {
+      final transactionId = await algorand.assetManager.createAsset(
+        account: account,
+        assetName: assetName,
+        unitName: unitName,
+        totalAssets: totalAssets,
+        decimals: decimals,
+      );
+      return transactionId;
+    } catch (e) {
+      debugPrint("Failed to create asset: $e");
+      throw Exception("Failed to create asset: $e");
+    }
+  }
+
+  // Edit an asset
+  Future<void> editAsset({
+    required int assetId,
+    required Account account,
+    required String managerAddress,
+    required String reserveAddress,
+    required String freezeAddress,
+    required String clawbackAddress,
+  }) async {
+    try {
+      // Convert string addresses to Address objects
+      Address manager = Address.fromAlgorandAddress(address: managerAddress);
+      Address reserve = Address.fromAlgorandAddress(address: reserveAddress);
+      Address freeze = Address.fromAlgorandAddress(address: freezeAddress);
+      Address clawback = Address.fromAlgorandAddress(address: clawbackAddress);
+
+      // Edit the asset using the AssetManager from the SDK
+      await algorand.assetManager.editAsset(
+        assetId: assetId,
+        account: account,
+        managerAddress: manager,
+        reserveAddress: reserve,
+        freezeAddress: freeze,
+        clawbackAddress: clawback,
+      );
+      debugPrint("Asset edited successfully: Asset ID $assetId");
+    } catch (e) {
+      debugPrint("Failed to edit asset: $e");
+      throw Exception("Failed to edit asset: $e");
+    }
+  }
+
+  // Destroy an asset
+  Future<void> destroyAsset(int assetId, Account account) async {
+    try {
+      await algorand.assetManager.destroyAsset(
+        assetId: assetId,
+        account: account,
+      );
+    } catch (e) {
+      debugPrint("Failed to destroy asset: $e");
+      throw Exception("Failed to destroy asset: $e");
+    }
+  }
+
+  // Opt-in to an asset
+  Future<void> optInAsset(int assetId, Account account) async {
+    try {
+      await algorand.assetManager.optIn(
+        assetId: assetId,
+        account: account,
+      );
+    } catch (e) {
+      debugPrint("Failed to opt-in to asset: $e");
+      throw Exception("Failed to opt-in to asset: $e");
+    }
+  }
+
+  // Transfer an asset
+  Future<void> transferAsset(int assetId, Account senderAccount,
+      String receiverAddress, int amount) async {
+    try {
+      await algorand.assetManager.transfer(
+        assetId: assetId,
+        account: senderAccount,
+        receiver: Address.fromAlgorandAddress(address: receiverAddress),
+        amount: amount,
+      );
+    } catch (e) {
+      debugPrint("Failed to transfer asset: $e");
+      throw Exception("Failed to transfer asset: $e");
+    }
+  }
+
+  // Freeze an asset
+  Future<void> freezeAsset(int assetId, Account freezeAccount,
+      String freezeTargetAddress, bool freeze) async {
+    try {
+      await algorand.assetManager.freeze(
+        assetId: assetId,
+        account: freezeAccount,
+        freezeTarget: Address.fromAlgorandAddress(address: freezeTargetAddress),
+        freeze: freeze,
+      );
+    } catch (e) {
+      debugPrint("Failed to freeze asset: $e");
+      throw Exception("Failed to freeze asset: $e");
+    }
+  }
+
+  // Revoke an asset
+  Future<void> revokeAsset(
+      int assetId, Account account, int amount, String revokeAddress) async {
+    try {
+      await algorand.assetManager.revoke(
+        assetId: assetId,
+        account: account,
+        amount: amount,
+        revokeAddress: Address.fromAlgorandAddress(address: revokeAddress),
+      );
+    } catch (e) {
+      debugPrint("Failed to revoke asset: $e");
+      throw Exception("Failed to revoke asset: $e");
     }
   }
 }
