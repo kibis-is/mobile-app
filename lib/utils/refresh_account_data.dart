f19bc6169644cbc4089e7de48730d5ca618202ed
David Welsh <contact@davidwelsh.net>
2024-06-17 14:44:30 +0700

    add loading overlay to switch accounts

--- lib/utils/refresh_account_data.dart
+++ lib/utils/refresh_account_data.dart
@@ -2,8 +2,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kibisis/providers/assets_provider.dart';
 import 'package:kibisis/providers/balance_provider.dart';
+import 'package:kibisis/providers/loading_provider.dart';
 
 void refreshAccountData(WidgetRef ref, String publicAddress) async {
+  ref.read(loadingProvider.notifier).startLoading(); // Start loading
+
   if (publicAddress.isNotEmpty) {
     debugPrint('Fetching assets for public address: $publicAddress');
     await ref.read(assetsProvider.notifier).getAccountAssets(publicAddress);
@@ -11,4 +14,6 @@ void refreshAccountData(WidgetRef ref, String publicAddress) async {
     debugPrint('Fetching balance for public address: $publicAddress');
     await ref.read(balanceProvider.notifier).getBalance(publicAddress);
   }
+
+  ref.read(loadingProvider.notifier).stopLoading(); // Stop loading
 }
