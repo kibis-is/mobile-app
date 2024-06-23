458efbbf07dda960ddaa30d411fa910e5cf5d66d
David Welsh <contact@davidwelsh.net>
2024-06-23 19:44:28 +0700

    use the styled asset tile for the search asset.

--- lib/features/add_asset/add_asset_screen.dart
+++ lib/features/add_asset/add_asset_screen.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:kibisis/common_widgets/asset_list_item.dart';
 import 'package:kibisis/common_widgets/custom_text_field.dart';
 import 'package:kibisis/constants/constants.dart';
-import 'package:kibisis/providers/active_asset_provider.dart';
 import 'package:kibisis/providers/algorand_provider.dart';
-import 'package:kibisis/routing/named_routes.dart';
 import 'package:kibisis/utils/theme_extensions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:algorand_dart/algorand_dart.dart';
@@ -95,28 +93,10 @@ class AddAssetScreen extends ConsumerWidget {
                     itemCount: assets.length,
                     itemBuilder: (context, index) {
                       final asset = assets[index];
-                      return ListTile(
-                          title: Text(asset.params.name ?? 'No name'),
-                          subtitle:
-                              Text(asset.params.unitName ?? 'No Unit Name'),
-                          onTap: () async {
-                            final assetDetails = await ref
-                                .read(algorandServiceProvider)
-                                .getAssetById(asset.index);
-                            ref
-                                .read(activeAssetProvider.notifier)
-                                .setActiveAsset(assetDetails);
-                            if (context.mounted) {
-                              context.goNamed(
-                                viewAssetRouteName,
-                                pathParameters: {
-                                  'mode': 'add',
-                                },
-                              );
-                            }
-                            ref.invalidate(searchProvider);
-                            accountController.text = '';
-                          });
+                      return AssetListItem(
+                        asset: asset,
+                        mode: AssetScreenMode.add,
+                      );
                     },
                   );
                 },
