915553e3c3ec16526389e2ccb4ceb83088e1cda9
David Welsh <contact@davidwelsh.net>
2024-07-16 22:40:38 +0100

    update app version to 0.1.0

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -7,7 +7,7 @@ const kWidgetRadius = 8.0;
 const kSizedBoxSpacing = 16.0;
 const kInputHeight = 64.0;
 const kPinLength = 6;
-const kVersionNumber = 'v0.0.1';
+const kVersionNumber = 'v0.1.0';
 
 enum PinPadMode { setup, unlock, verifyTransaction }
 
