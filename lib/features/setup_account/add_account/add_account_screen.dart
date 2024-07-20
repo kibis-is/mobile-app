d6262763b06a214eac8b1184997bf63aed1d70e3
David Welsh <contact@davidwelsh.net>
2024-07-20 09:36:49 +0100

    fix scoll issue

--- lib/features/setup_account/add_account/add_account_screen.dart
+++ lib/features/setup_account/add_account/add_account_screen.dart
@@ -29,73 +29,75 @@ class AddAccountScreenState extends ConsumerState<AddAccountScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(kScreenPadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'You can either create a new account or import an existing account.',
-              style: context.textTheme.bodySmall,
-            ),
-            const SizedBox(
-              height: kScreenPadding,
-            ),
-            CustomListTile(
-              title: "Create New Account",
-              subtitle: 'You will be prompted to save a seed.',
-              leadingIcon: AppIcons.addAccount,
-              trailingIcon: AppIcons.arrowRight,
-              onTap: () async {
-                await _createNewAccount(ref);
-              },
-            ),
-            const SizedBox(
-              height: kScreenPadding,
-            ),
-            CustomListTile(
-              title: "Import Via Seed",
-              subtitle: 'Import an existing account via seed phrase.',
-              leadingIcon: AppIcons.importAccount,
-              trailingIcon: AppIcons.arrowRight,
-              onTap: () {
-                GoRouter.of(context).push(
-                    widget.accountFlow == AccountFlow.setup
-                        ? '/setup/setupImportSeed'
-                        : '/addAccount/addAccountImportSeed');
-              },
-            ),
-            if (defaultTargetPlatform == TargetPlatform.android ||
-                defaultTargetPlatform == TargetPlatform.iOS)
-              Column(
-                children: [
-                  const SizedBox(
-                    height: kScreenPadding,
-                  ),
-                  CustomListTile(
-                    title: "Import Via QR Code",
-                    subtitle: 'Scan a QR code to import an existing account.',
-                    leadingIcon: AppIcons.scan,
-                    trailingIcon: AppIcons.arrowRight,
-                    onTap: _navigateToImportViaQr,
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'You can either create a new account or import an existing account.',
+                style: context.textTheme.bodySmall,
               ),
-            if (kDebugMode)
-              Column(
-                children: [
-                  const SizedBox(
-                    height: kScreenPadding,
-                  ),
-                  CustomListTile(
-                    title: "Import Hardcoded URI",
-                    subtitle:
-                        'Import accounts from a hardcoded URI for testing.',
-                    leadingIcon: AppIcons.importAccount,
-                    trailingIcon: AppIcons.arrowRight,
-                    onTap: _importFromHardcodedUri,
-                  ),
-                ],
+              const SizedBox(
+                height: kScreenPadding,
               ),
-          ],
+              CustomListTile(
+                title: "Create New Account",
+                subtitle: 'You will be prompted to save a seed.',
+                leadingIcon: AppIcons.addAccount,
+                trailingIcon: AppIcons.arrowRight,
+                onTap: () async {
+                  await _createNewAccount(ref);
+                },
+              ),
+              const SizedBox(
+                height: kScreenPadding,
+              ),
+              CustomListTile(
+                title: "Import Via Seed",
+                subtitle: 'Import an existing account via seed phrase.',
+                leadingIcon: AppIcons.importAccount,
+                trailingIcon: AppIcons.arrowRight,
+                onTap: () {
+                  GoRouter.of(context).push(
+                      widget.accountFlow == AccountFlow.setup
+                          ? '/setup/setupImportSeed'
+                          : '/addAccount/addAccountImportSeed');
+                },
+              ),
+              if (defaultTargetPlatform == TargetPlatform.android ||
+                  defaultTargetPlatform == TargetPlatform.iOS)
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: kScreenPadding,
+                    ),
+                    CustomListTile(
+                      title: "Import Via QR Code",
+                      subtitle: 'Scan a QR code to import an existing account.',
+                      leadingIcon: AppIcons.scan,
+                      trailingIcon: AppIcons.arrowRight,
+                      onTap: _navigateToImportViaQr,
+                    ),
+                  ],
+                ),
+              if (kDebugMode)
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: kScreenPadding,
+                    ),
+                    CustomListTile(
+                      title: "Import Hardcoded URI",
+                      subtitle:
+                          'Import accounts from a hardcoded URI for testing.',
+                      leadingIcon: AppIcons.importAccount,
+                      trailingIcon: AppIcons.arrowRight,
+                      onTap: _importFromHardcodedUri,
+                    ),
+                  ],
+                ),
+            ],
+          ),
         ),
       ),
     );
