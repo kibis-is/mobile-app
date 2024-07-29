2b6679784463458f99d7771ecc31ab3669fcc34c
David Welsh <contact@davidwelsh.net>
2024-07-29 19:56:12 +0100

    Create a progress bar

---
+++ lib/features/scan_qr/widgets/progress_bar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kibisis/constants/constants.dart';
+import 'package:kibisis/utils/theme_extensions.dart';
+
+final progressBarProvider = StateProvider<double>((ref) => 0.0);
+
+class AnimatedProgressBar extends ConsumerWidget {
+  const AnimatedProgressBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final progress = ref.watch(progressBarProvider);
+    return Container(
+      width: double.infinity,
+      height: kScreenPadding * 2,
+      decoration: BoxDecoration(
+        color: context.colorScheme.surface,
+        borderRadius: BorderRadius.circular(kWidgetRadius),
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        fit: StackFit.loose,
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: FractionallySizedBox(
+              widthFactor: progress,
+              child: Container(
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      context.colorScheme.primary,
+                      context.colorScheme.secondary
+                    ],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                  borderRadius: BorderRadius.circular(kWidgetRadius),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              '${(progress * 100).toStringAsFixed(0)}%',
+              style: TextStyle(color: context.colorScheme.onSurface),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
