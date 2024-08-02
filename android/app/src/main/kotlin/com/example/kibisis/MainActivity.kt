93780769815d9d7fd41a80e5ba9cdb01d778f942
David Welsh <contact@davidwelsh.net>
2024-08-02 18:49:58 +0100

    update the app name

--- android/app/src/main/kotlin/com/example/kibisis/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package is.kibis.kibisis
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/is/kibis/kibisis/MainActivity.kt
@@ -0,0 +1,5 @@
+package `is`.kibis.kibisis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
