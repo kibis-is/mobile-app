e1914a6a9d82154bb6157ffa28d5db8061134caf
David Welsh <contact@davidwelsh.net>
2024-08-12 10:19:31 +0100

    add nft provider to invalidateProviders function

--- lib/utils/refresh_account_data.dart
+++ lib/utils/refresh_account_data.dart
@@ -3,9 +3,11 @@ import 'package:kibisis/features/dashboard/providers/assets_fetched_provider.dar
 import 'package:kibisis/features/dashboard/providers/transactions_provider.dart';
 import 'package:kibisis/providers/assets_provider.dart';
 import 'package:kibisis/providers/balance_provider.dart';
+import 'package:kibisis/providers/nft_provider.dart';
 
 void invalidateProviders(WidgetRef ref) {
   ref.invalidate(accountDataFetchStatusProvider);
+  ref.invalidate(nftNotifierProvider);
   ref.invalidate(balanceProvider);
   ref.invalidate(assetsProvider);
   ref.invalidate(transactionsProvider);
