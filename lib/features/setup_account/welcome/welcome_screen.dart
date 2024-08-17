f55bbc3207667b4d57b68487c5bfc9e0489a6a94
David Welsh <contact@davidwelsh.net>
2024-08-17 11:37:14 +0100

    set the title provider to solve bug

--- lib/features/setup_account/welcome/welcome_screen.dart
+++ lib/features/setup_account/welcome/welcome_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart' hide Orientation;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kibisis/common_widgets/custom_button.dart';
 import 'package:kibisis/constants/constants.dart';
+import 'package:kibisis/features/pin_pad/providers/pin_title_provider.dart';
 import 'package:kibisis/utils/theme_extensions.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends ConsumerWidget {
   static String title = "Login";
   const WelcomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     String kibisisLogo = Theme.of(context).brightness == Brightness.dark
         ? 'assets/images/kibisis-logo-dark.svg'
         : 'assets/images/kibisis-logo-light.svg';
@@ -56,6 +58,7 @@ class WelcomeScreen extends StatelessWidget {
                       text: 'Create Pin',
                       isFullWidth: true,
                       onPressed: () {
+                        ref.read(pinTitleProvider.notifier).setCreatePinTitle();
                         GoRouter.of(context).push('/setup/pinPadSetup');
                       },
                     )
