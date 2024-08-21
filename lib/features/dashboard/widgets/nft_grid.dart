66a51d7ef232cad1ea7bc28a27d1e0af75f18c35
David Welsh <contact@davidwelsh.net>
2024-08-21 22:36:57 +0100

    refactoring

---
+++ lib/features/dashboard/widgets/nft_grid.dart
@@ -0,0 +1,166 @@
+import 'package:ellipsized_text/ellipsized_text.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:kibisis/models/nft.dart';
+import 'package:kibisis/constants/constants.dart';
+import 'package:kibisis/routing/named_routes.dart';
+import 'package:kibisis/theme/color_palette.dart';
+import 'package:kibisis/utils/theme_extensions.dart';
+
+enum NftViewType { grid, card }
+
+class NftGridOrCard extends StatelessWidget {
+  final List<NFT> nfts;
+  final NftViewType viewType;
+
+  const NftGridOrCard({
+    super.key,
+    required this.nfts,
+    required this.viewType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: viewType == NftViewType.grid ? 3 : 1,
+          childAspectRatio: 1.0,
+        ),
+        itemCount: nfts.length,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          final nft = nfts[index];
+          return NftCard(
+            nft: nft,
+            viewType: viewType,
+            index: index,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class NftCard extends StatelessWidget {
+  final NFT nft;
+  final NftViewType viewType;
+  final int index;
+
+  const NftCard({
+    super.key,
+    required this.nft,
+    required this.viewType,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.pushNamed(
+          viewNftRouteName,
+          pathParameters: {'index': index.toString()},
+        );
+      },
+      child: Card(
+        margin: viewType == NftViewType.grid
+            ? const EdgeInsets.all(0)
+            : const EdgeInsets.only(bottom: kScreenPadding / 2),
+        shape: RoundedRectangleBorder(
+          borderRadius: viewType == NftViewType.grid
+              ? BorderRadius.circular(0)
+              : BorderRadius.circular(kWidgetRadius),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: Stack(
+          children: [
+            NftImage(imageUrl: nft.imageUrl),
+            const NftImageOverlay(),
+            NftDetails(
+              name: nft.name,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NftImage extends StatelessWidget {
+  final String imageUrl;
+
+  const NftImage({
+    super.key,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      imageUrl,
+      fit: BoxFit.cover,
+      width: double.infinity,
+      height: double.infinity,
+    );
+  }
+}
+
+class NftImageOverlay extends StatelessWidget {
+  const NftImageOverlay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.center,
+            colors: [
+              Colors.black87,
+              Colors.transparent,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NftDetails extends StatelessWidget {
+  final String name;
+
+  const NftDetails({
+    super.key,
+    required this.name,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Padding(
+        padding: const EdgeInsets.all(kScreenPadding / 2),
+        child: SizedBox(
+          width: double.infinity,
+          child: EllipsizedText(
+            name,
+            style: context.textTheme.bodySmall?.copyWith(
+              fontWeight: FontWeight.bold,
+              color: ColorPalette.darkThemeWhite,
+              shadows: const [
+                Shadow(
+                  offset: Offset(1.0, 1.0),
+                  blurRadius: 2.0,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/dashboard/widgets/nft_tab.dart
+++ lib/features/dashboard/widgets/nft_tab.dart
@@ -1,22 +1,16 @@
-import 'package:ellipsized_text/ellipsized_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kibisis/common_widgets/custom_pull_to_refresh.dart';
 import 'package:kibisis/common_widgets/custom_text_field.dart';
 import 'package:kibisis/constants/constants.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:kibisis/models/nft.dart';
+import 'package:kibisis/features/dashboard/widgets/nft_grid.dart';
 import 'package:kibisis/providers/account_provider.dart';
 import 'package:kibisis/providers/nft_provider.dart';
-import 'package:kibisis/routing/named_routes.dart';
-import 'package:kibisis/theme/color_palette.dart';
 import 'package:kibisis/utils/app_icons.dart';
 import 'package:kibisis/utils/theme_extensions.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:shimmer/shimmer.dart';
-
-enum NftViewType { grid, card }
 
 class NftTab extends ConsumerStatefulWidget {
   const NftTab({super.key});
@@ -64,26 +58,28 @@ class NftTabState extends ConsumerState<NftTab> {
   Widget build(BuildContext context) {
     final nftState = ref.watch(nftNotifierProvider);
 
-    return CustomPullToRefresh(
-      refreshController: _refreshController,
-      onRefresh: _onRefresh,
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            titleSpacing: 0,
-            floating: true,
-            snap: true,
-            title: _buildSearchBar(context),
+    return Column(
+      children: [
+        _buildSearchBar(context),
+        const SizedBox(height: kScreenPadding / 4),
+        Expanded(
+          child: CustomPullToRefresh(
+            refreshController: _refreshController,
+            onRefresh: _onRefresh,
+            child: CustomScrollView(
+              slivers: [
+                nftState.when(
+                  data: (nfts) => nfts.isEmpty
+                      ? _buildEmptyNfts(context)
+                      : NftGridOrCard(nfts: nfts, viewType: viewType),
+                  loading: () => _buildLoadingNfts(context),
+                  error: (error, stack) => _buildEmptyNfts(context),
+                ),
+              ],
+            ),
           ),
-          nftState.when(
-            data: (nfts) => nfts.isEmpty
-                ? _buildEmptyNfts(context)
-                : _buildNftGridOrCard(nfts),
-            loading: () => _buildLoadingNfts(context),
-            error: (error, stack) => _buildEmptyNfts(context),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
@@ -116,89 +112,6 @@ class NftTabState extends ConsumerState<NftTab> {
           ),
         ),
       ],
-    );
-  }
-
-  Widget _buildNftGridOrCard(List<NFT> nfts) {
-    return SliverToBoxAdapter(
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: viewType == NftViewType.grid ? 3 : 1,
-          childAspectRatio: 1.0,
-        ),
-        itemCount: nfts.length,
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemBuilder: (context, index) {
-          final nft = nfts[index];
-          return GestureDetector(
-            onTap: () {
-              context.pushNamed(
-                viewNftRouteName,
-                pathParameters: {'index': index.toString()},
-              );
-            },
-            child: Card(
-              margin: viewType == NftViewType.grid
-                  ? const EdgeInsets.all(0)
-                  : const EdgeInsets.only(bottom: kScreenPadding / 2),
-              shape: RoundedRectangleBorder(
-                borderRadius: viewType == NftViewType.grid
-                    ? BorderRadius.circular(0)
-                    : BorderRadius.circular(kWidgetRadius),
-              ),
-              clipBehavior: Clip.antiAlias,
-              child: Stack(
-                children: [
-                  Image.network(
-                    nft.imageUrl,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    height: double.infinity,
-                  ),
-                  Positioned.fill(
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.center,
-                          colors: [
-                            Colors.black87,
-                            Colors.transparent,
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.all(kScreenPadding / 2),
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: EllipsizedText(
-                          nft.name,
-                          style: context.textTheme.bodySmall?.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: ColorPalette.darkThemeWhite,
-                            shadows: const [
-                              Shadow(
-                                offset: Offset(1.0, 1.0),
-                                blurRadius: 2.0,
-                                color: Colors.black,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
     );
   }
 
