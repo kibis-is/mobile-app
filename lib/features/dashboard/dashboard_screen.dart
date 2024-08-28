94b360e9b9af8b367daa895289bb39fdd3d25d2a
David Welsh <contact@davidwelsh.net>
2024-08-28 19:41:09 +0100

    remove unused import

--- lib/features/dashboard/dashboard_screen.dart
+++ lib/features/dashboard/dashboard_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:ellipsized_text/ellipsized_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
