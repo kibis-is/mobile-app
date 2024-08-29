b3695571f19a6f39325b882a46fd390bdfe99c00
Kieran O'Neill <hello@kieranoneill.com>
2024-08-29 13:07:14 +0100

    feat: add release workflow and commit linting (#2)
    * build(deps): add commitlint_cli and jusky packages
    
    * build: add commit message hook to lint commit messages
    
    * ci: add pull request workflow
    
    * docs: add contributing guide
    
    * chore: update gitignore
    
    * ci: add release workflow that updates version in yaml
    
    * feat: add nunito fints
    
    * refactor: rename license to copying
    
    * fix: move kotlin version to global gradle file
    
    * docs: add readme and repo assets
    
    * chore: squash
    
    * chore: squash
    
    * ci: use a debug build for pr checks workflow
    
    * docs: update readme docs to incorporate keys
    
    * ci: add partial implementation of deployment on release

---
+++ scripts/create_android_signing_keys.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname "${0}")
+
+source "${SCRIPT_DIR}/set_vars.sh"
+
+# Public: Creates the Android signing keys necessary for signing a release.
+#
+# Required environment variables:
+# * $ANDROID_KEY_ALIAS - The alias for the key.
+# * $ANDROID_KEY_PASSWORD - The password for the key.
+# * $ANDROID_KEYSTORE - The base64-encoded content of the keystore file.
+# * $ANDROID_KEYSTORE_PASSWORD - The password for the keystore.
+#
+# Examples
+#
+#   ./scripts/create_android_signing_keys.sh
+#
+# Returns exit code 0 if successful.
+function main {
+  set_vars
+
+  # decode the base64-encoded keystore and write it to a .jks file at the project root
+  printf "%b decoding the base64-encoded key store to the keystore file... \n" "${INFO_PREFIX}"
+  echo "${ANDROID_KEYSTORE}" | base64 -d > "key.jks"
+
+  # generate key.properties
+  printf "%b creating key.properties file... \n" "${INFO_PREFIX}"
+  {
+    echo "keyAlias=${ANDROID_KEY_ALIAS}"
+    echo "keyPassword=${ANDROID_KEY_PASSWORD}"
+    echo "storeFile=${PWD}/key.jks"
+    echo "storePassword=${ANDROID_KEYSTORE_PASSWORD}"
+  } > "android/key.properties"
+
+  exit 0
+}
+
+# and so, it begins...
+main
