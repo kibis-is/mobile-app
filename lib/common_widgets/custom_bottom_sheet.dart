ddce1bd25962707fb577b3d93ebcd1b36bedae47
David Welsh <contact@davidwelsh.net>
2024-10-17 16:27:52 +0700

    fix: Asset select field on the Send Transaction screen now fixed to handle long lists of items.

--- lib/common_widgets/custom_bottom_sheet.dart
+++ lib/common_widgets/custom_bottom_sheet.dart
@@ -20,36 +20,43 @@ Future<dynamic> customBottomSheet({
 
   return showModalBottomSheet(
     context: context,
-    isScrollControlled:
-        true, // Allows the bottom sheet to shrinkwrap its content
+    isScrollControlled: true,
     builder: (BuildContext context) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(
-            horizontal: kScreenPadding * 2, vertical: kScreenPadding),
-        child: Column(
-          mainAxisSize:
-              MainAxisSize.min, // Ensure the column takes only needed space
-          children: [
-            _buildHeader(context, header),
-            const SizedBox(height: kScreenPadding),
-            if (isSingleWidgetMode)
-              singleWidget
-            else
-              _buildItemList(context, items!, onPressed!),
-            if (hasButton)
-              CustomButton(
-                text: buttonText ?? "Confirm",
-                onPressed: () {
-                  if (buttonOnPressed != null) {
-                    buttonOnPressed();
-                  }
-                  Navigator.of(context).pop();
-                },
-                isFullWidth: true,
+      return DraggableScrollableSheet(
+        expand: false,
+        builder: (context, scrollController) {
+          return SingleChildScrollView(
+            controller: scrollController,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: kScreenPadding * 2, vertical: kScreenPadding),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  _buildHeader(context, header),
+                  const SizedBox(height: kScreenPadding),
+                  if (isSingleWidgetMode)
+                    singleWidget
+                  else
+                    _buildItemList(context, items!, onPressed!),
+                  if (hasButton)
+                    CustomButton(
+                      text: buttonText ?? "Confirm",
+                      onPressed: () {
+                        if (buttonOnPressed != null) {
+                          buttonOnPressed();
+                        }
+                        Navigator.of(context).pop();
+                      },
+                      isFullWidth: true,
+                    ),
+                  if (isSingleWidgetMode)
+                    const SizedBox(height: kScreenPadding),
+                ],
               ),
-            if (isSingleWidgetMode) const SizedBox(height: kScreenPadding),
-          ],
-        ),
+            ),
+          );
+        },
       );
     },
   );
@@ -58,7 +65,9 @@ Future<dynamic> customBottomSheet({
 Widget _buildHeader(BuildContext context, String header) {
   return Padding(
     padding: const EdgeInsets.all(kSizedBoxSpacing),
-    child: EllipsizedText(header, style: context.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold)),
+    child: EllipsizedText(header,
+        style: context.textTheme.titleMedium
+            ?.copyWith(fontWeight: FontWeight.bold)),
   );
 }
 
@@ -67,11 +76,9 @@ Widget _buildItemList(
   List<dynamic> items,
   Function(SelectItem) onPressed,
 ) {
-  // Removed the Expanded here to allow the ListView to shrinkwrap
   return ListView.builder(
-    shrinkWrap: true, // Ensures the ListView takes up only the necessary space
-    physics:
-        const NeverScrollableScrollPhysics(), // Prevents scrolling inside the sheet
+    shrinkWrap: true,
+    physics: const BouncingScrollPhysics(),
     itemCount: items.length,
     itemBuilder: (BuildContext context, int index) {
       return _buildListItem(context, items[index], onPressed);
@@ -99,9 +106,11 @@ Widget _buildListItem(
             color: context.colorScheme.primary,
           ),
           const SizedBox(width: kSizedBoxSpacing),
-          Text(
-            item.name,
-            style: context.textTheme.displayMedium,
+          Flexible(
+            child: EllipsizedText(
+              item.name,
+              style: context.textTheme.displayMedium,
+            ),
           ),
         ],
       ),
