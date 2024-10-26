d3d736686b0146cb977d2c9e34d3745c5389aabc
David Welsh <contact@davidwelsh.net>
2024-10-26 14:59:13 +0700

    fix: correctly process long integers for displaying balance

--- lib/utils/number_shortener.dart
+++ lib/utils/number_shortener.dart
@@ -1,18 +1,17 @@
 class NumberShortener {
   static String shortenNumber(double number) {
-    if (number < 1000000) {
-      return number % 1 == 0 ? number.toStringAsFixed(0) : number.toStringAsFixed(2);
-    }
+    const suffixes = ['', 'K', 'M', 'B', 'T'];
+    int suffixIndex = 0;
 
-    const suffixes = ['M', 'B', 'T'];
-    int suffixIndex = -1;
-
-    while (number >= 1000000 && suffixIndex < suffixes.length - 1) {
+    while (number >= 1000 && suffixIndex < suffixes.length - 1) {
       number /= 1000;
       suffixIndex++;
     }
 
-    // Ensure only 2 decimal places max
-    return '${number.toStringAsFixed(number % 1 == 0 ? 0 : 2)}${suffixes[suffixIndex]}';
+    // Format number with up to two decimal places
+    String formattedNumber =
+        number % 1 == 0 ? number.toStringAsFixed(0) : number.toStringAsFixed(2);
+
+    return '$formattedNumber${suffixes[suffixIndex]}';
   }
 }
