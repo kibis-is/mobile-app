45b743d3645ebc26696104e437a870100df3142f
David Welsh <contact@davidwelsh.net>
2024-11-25 12:44:57 +0700

    refactor: remove unused import

--- lib/utils/refresh_account_data.dart
+++ lib/utils/refresh_account_data.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kibisis/features/dashboard/providers/assets_fetched_provider.dart';
 import 'package:kibisis/features/dashboard/providers/transactions_provider.dart';
 import 'package:kibisis/features/pin_pad/providers/pin_title_provider.dart';
 import 'package:kibisis/providers/accounts_list_provider.dart';
